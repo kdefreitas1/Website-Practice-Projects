74d0b096e7ac8f33e33be592d9c0c39a18543a2f
Kamalani DeFreitas <109827214+kdefreitas1@users.noreply.github.com>
2025-09-23 12:43:09 -0400

    Completed fetch pokemon

--- fetch-pokemon/fetch-pokemon.js
+++ fetch-pokemon/fetch-pokemon.js
@@ -1,17 +1,20 @@
 async function fetchData(){
     try{
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/typhlosion");
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         
         if(!response.ok){
             throw new Error("Could not fetch resource");
         }
 
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
     catch(error){
         console.error(error);
     }
 }
-
-fetchData();
